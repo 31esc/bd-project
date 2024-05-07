eefd175326b1657bdc2e067fd9e27ebf11db926f
Bobrov Aleksey <bobrov.aa@phystech.edu>
2024-05-07 15:46:53 +0300

    fix index

--- scripts/index.sql
+++ scripts/index.sql
@@ -1,5 +1,3 @@
 CREATE INDEX idx_trip_user_id ON project.trip_details (user_id);
 CREATE INDEX idx_trip_driver_id ON project.trip_details (driver_id);
-CREATE INDEX idx_driver_id ON project.driver (driver_id);
-CREATE INDEX idx_user_id ON project.user (user_id);
-CREATE INDEX idx_car_model ON project.car (model);
+CREATE INDEX idx_trip_end_time ON project.trip_details (end_time);
